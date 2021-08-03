378d3c30a8b8aab69a8ac4b1caa6d6f3f2994aba
thewilkupl <thewilku@o2.pl>
2021-08-03 08:52:51 +0200

    add full content

--- src/utils/navbarlist.js
+++ src/utils/navbarlist.js
@@ -36,3 +36,24 @@ export const socials = [
     url: "/#",
   },
 ];
+
+export const additionals = [
+  {
+    id: 1,
+    text: "about",
+    url: "/about",
+    active: true,
+  },
+  {
+    id: 2,
+    text: "latest work",
+    url: "/work",
+    active: true,
+  },
+  {
+    id: 3,
+    text: "blog",
+    url: "/blog",
+    active: false,
+  },
+];
