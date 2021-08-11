64f108872ab066498d08b2042c67e6d7f0c9ebdb
thewilkupl <thewilku@o2.pl>
2021-08-11 11:24:55 +0200

    handling error page / fix h1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -88,39 +88,12 @@ const SignWrapper = styled.div`
   h1 {
     font-size: 2.5rem;
     color: #fff;
-    animation: type 3s steps(18);
-    overflow: hidden;
-    white-space: nowrap;
-    font-family: consolas;
-    width: 18ch;
   }
 
   span {
     color: #fff;
     animation: blink 1s linear infinite;
     font-size: 3rem;
-  }
-
-  @keyframes blink {
-    0% {
-      opacity: 1;
-    }
-    50% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-
-  @keyframes type {
-    0% {
-      width: 0ch;
-    }
-
-    100% {
-      width: 18ch;
-    }
   }
 
   @media only screen and (max-width: 1024px) {
--- src/sites/Error.tsx
+++ src/sites/Error.tsx
@@ -1,11 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+//images
+import error from "../assets/error.png";
+//link to
+import { Link } from "react-router-dom";
 
 const Error: React.FC = () => {
   return (
-    <div>
-      <h1>error</h1>
-    </div>
+    <Wrapper>
+      <Navbar />
+      <div className="section">
+        <h1>404</h1>
+        <p>The page you are looking for does not exist!</p>
+        <img src={error} alt="error" />
+        <Link to="/">Go back home</Link>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .section {
+    height: 80vh;
+    width: 80vw;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+
+    img {
+      object-fit: contain;
+      height: 20rem;
+      width: 20rem;
+    }
+
+    h1 {
+      font-size: 5rem;
+    }
+
+    a {
+      background: gray;
+      padding: 1rem;
+      color: #ffffff;
+      font-weight: bold;
+      width: max-content;
+      margin-top: 1.5rem;
+      margin-bottom: 1.5rem;
+      font-size: 1.2rem;
+      border-radius: 5px;
+    }
+
+    a:after {
+      width: 0;
+    }
+
+    a:hover {
+      background: black;
+    }
+  }
+`;
 
 export default Error;
