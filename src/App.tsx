4d3b44c8b592fe03690095e6a10b9c8f099959af
thewilkupl <thewilku@o2.pl>
2021-10-29 11:42:27 +0200

    performance fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import GlobalStyle from "./globalstyles";
 import Home from "./sites/Home";
-import About from "./sites/About";
 import Error from "./sites/Error";
-import Work from "./sites/Work";
+import Spinner from "./components/Spinner";
+
+const About = lazy(() => import("./sites/About"));
+const Work = lazy(() => import("./sites/Work"));
 
 const App: React.FC = () => {
   //Animacja przejścia między stronami
@@ -15,20 +17,22 @@ const App: React.FC = () => {
     <div className="App">
       <GlobalStyle />
       <AnimatePresence>
-        <Switch location={location} key={location.pathname}>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/about" exact>
-            <About />
-          </Route>
-          <Route path="/work" exact>
-            <Work />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Suspense fallback={<Spinner />}>
+          <Switch location={location} key={location.pathname}>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about" exact>
+              <About />
+            </Route>
+            <Route path="/work" exact>
+              <Work />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+        </Suspense>
       </AnimatePresence>
     </div>
   );
